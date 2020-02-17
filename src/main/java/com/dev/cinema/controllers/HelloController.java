4adb1880f130d33a95e2cd57e43e3c3e759eeb50
Dmitrii <zinchukdimaa@gmail.com>
2020-02-17 16:38:04 +0200

    change to RestController

--- src/main/java/com/dev/cinema/controllers/HelloController.java
+++ src/main/java/com/dev/cinema/controllers/HelloController.java
@@ -1,13 +1,13 @@
 package com.dev.cinema.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping("/Hello")
     public String sayHello() {
-        return "/Hello";
+        return "Hello";
     }
 }
--- src/main/webapp/WEB-INF/view/Hello.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Hello</title>
-</head>
-<body>
-<h1>Hello, mates!</h1>
-</body>
-</html>
