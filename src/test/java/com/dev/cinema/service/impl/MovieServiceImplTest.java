bfad0c5b9c84a6a733cadfa448025c7bd7d9173a
Dmitrii <zinchukdimaa@gmail.com>
2020-04-07 12:01:37 +0300

    smth

--- src/test/java/com/dev/cinema/service/impl/MovieServiceImplTest.java
+++ src/test/java/com/dev/cinema/service/impl/MovieServiceImplTest.java
@@ -20,11 +20,11 @@ import org.mockito.MockitoAnnotations;
 
 class MovieServiceImplTest {
 
-    private static Movie first;
-    private static Movie second;
-    private static Movie third;
+    private static Movie firstMovie;
+    private static Movie secondMovie;
+    private static Movie thirdMovie;
     private static List<Movie> movieStorage;
-    private static Movie mockMovie;
+    private static Movie expectedMockMovie;
     private static Movie testMovie;
 
     @InjectMocks
@@ -35,34 +35,34 @@ class MovieServiceImplTest {
 
     @BeforeAll
     static void beforeAll() {
-        mockMovie = new Movie();
-        mockMovie.setId(1L);
-        mockMovie.setTitle("Test");
-        mockMovie.setDescription("Test");
+        expectedMockMovie = new Movie();
+        expectedMockMovie.setId(1L);
+        expectedMockMovie.setTitle("Test");
+        expectedMockMovie.setDescription("Test");
 
         testMovie = new Movie();
         testMovie.setTitle("Test");
         testMovie.setDescription("Test");
 
-        first = new Movie();
-        first.setId(1L);
-        first.setTitle("First");
-        first.setDescription("First");
+        firstMovie = new Movie();
+        firstMovie.setId(1L);
+        firstMovie.setTitle("First");
+        firstMovie.setDescription("First");
 
-        second = new Movie();
-        second.setId(2L);
-        second.setTitle("Second");
-        second.setDescription("Second");
+        secondMovie = new Movie();
+        secondMovie.setId(2L);
+        secondMovie.setTitle("Second");
+        secondMovie.setDescription("Second");
 
-        third = new Movie();
-        third.setId(3L);
-        third.setTitle("Third");
-        third.setDescription("Third");
+        thirdMovie = new Movie();
+        thirdMovie.setId(3L);
+        thirdMovie.setTitle("Third");
+        thirdMovie.setDescription("Third");
 
         movieStorage = new ArrayList<>();
-        movieStorage.add(first);
-        movieStorage.add(second);
-        movieStorage.add(third);
+        movieStorage.add(firstMovie);
+        movieStorage.add(secondMovie);
+        movieStorage.add(thirdMovie);
     }
 
     @BeforeEach
@@ -72,7 +72,7 @@ class MovieServiceImplTest {
 
     @Test
     void addMovieOk() {
-        when(movieDao.add(testMovie)).thenReturn(mockMovie);
+        when(movieDao.add(testMovie)).thenReturn(expectedMockMovie);
 
         Movie added = movieService.add(testMovie);
 
@@ -90,8 +90,8 @@ class MovieServiceImplTest {
 
         assertNotNull(movies);
         assertEquals(3, movies.size());
-        assertEquals(third.getId(), movies.get(2).getId());
-        assertEquals(third, movies.get(2));
+        assertEquals(thirdMovie.getId(), movies.get(2).getId());
+        assertEquals(thirdMovie, movies.get(2));
     }
 
     @Test
@@ -101,6 +101,6 @@ class MovieServiceImplTest {
         Movie movieById = movieService.getById(1L);
 
         verify(movieDao, times(1)).getById(any());
-        assertEquals(first, movieById);
+        assertEquals(firstMovie, movieById);
     }
 }
